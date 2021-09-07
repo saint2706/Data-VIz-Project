041eaf77a448141d307398e940931af2c7db970d
SAIN'T <arishabh748@gmail.com>
2021-09-07 17:48:37 +0530

    small update

--- PythonProjectReview1/regress.py
+++ PythonProjectReview1/regress.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 plt.style.use('ggplot')
 
 import statsmodels.api as sm
@@ -11,13 +12,13 @@ import statsmodels.stats.api as sms
 from statsmodels.formula.api import ols
 from scipy.stats import zscore
 from statsmodels.stats.stattools import durbin_watson
-from sklearn.model_selection import train_test_split,KFold
+from sklearn.model_selection import train_test_split, KFold
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn.metrics import mean_squared_error
 from sklearn.feature_selection import RFECV
 from mlxtend.feature_selection import SequentialFeatureSelector as sfs
 from mlxtend.plotting import plot_sequential_feature_selection as plot_sfs
-from sklearn.linear_model import LinearRegression,RidgeCV,LassoCV,ElasticNetCV
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV, ElasticNetCV
 
 # Objective
 target = 'area'
@@ -33,19 +34,19 @@ print(df.describe().T)
 print(df.isna().sum().sum())
 
 # Exploratory Data Analysis
-plt.rcParams["figure.figsize"] = 9,5
+plt.rcParams["figure.figsize"] = 9, 5
 
 # Univariate
-plt.figure(figsize=(16,5))
+plt.figure(figsize=(16, 5))
 print("Skew: {}".format(df[target].skew()))
 print("Kurtosis: {}".format(df[target].kurtosis()))
-ax = sns.kdeplot(df[target],shade=True,color='g')
-plt.xticks([i for i in range(0,1200,50)])
+ax = sns.kdeplot(df[target], shade=True, color='g')
+plt.xticks([i for i in range(0, 1200, 50)])
 plt.show()
 ax = sns.boxplot(df[target])
 
 # Outlier points
-y_outliers = df[abs(zscore(df[target])) >= 3 ]
+y_outliers = df[abs(zscore(df[target])) >= 3]
 print(y_outliers)
 
 # Independent Columns
@@ -56,26 +57,25 @@ print(cat_columns)
 print(num_columns)
 
 # analyzing categorical columns
-plt.figure(figsize=(16,10))
-for i,col in enumerate(cat_columns,1):
-    plt.subplot(2,2,i)
-    sns.countplot(data=dfa,y=col)
-    plt.subplot(2,2,i+2)
+plt.figure(figsize=(16, 10))
+for i, col in enumerate(cat_columns, 1):
+    plt.subplot(2, 2, i)
+    sns.countplot(data=dfa, y=col)
+    plt.subplot(2, 2, i + 2)
     df[col].value_counts(normalize=True).plot.bar()
     plt.ylabel(col)
     plt.xlabel('% distribution per category')
 plt.tight_layout()
-plt.show()    
+plt.show()
 
 # analyzing numerical columns
-plt.figure(figsize=(18,40))
-for i,col in enumerate(num_columns,1):
-    plt.subplot(8,4,i)
-    sns.kdeplot(df[col],color='g',shade=True)
-    plt.subplot(8,4,i+10)
+plt.figure(figsize=(18, 40))
+for i, col in enumerate(num_columns, 1):
+    plt.subplot(8, 4, i)
+    sns.kdeplot(df[col], color='g', shade=True)
+    plt.subplot(8, 4, i + 10)
     df[col].plot.box()
-plt.tight_layout() 
+plt.tight_layout()
 plt.show()
 num_data = df[num_columns]
-print(pd.DataFrame(data=[num_data.skew(),num_data.kurtosis()],index=['skewness','kurtosis']))
-
+print(pd.DataFrame(data=[num_data.skew(), num_data.kurtosis()], index=['skewness', 'kurtosis']))
