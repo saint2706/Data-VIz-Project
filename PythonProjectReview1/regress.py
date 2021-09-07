1f3301b6c68d136aec584d0ddcdb7ace29282b6a
SAIN'T <arishabh748@gmail.com>
2021-09-07 17:20:47 +0530

    Python time

--- PythonProjectReview1/regress.py
+++ PythonProjectReview1/regress.py
@@ -1,3 +1,4 @@
+# Dependencies
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,11 +19,28 @@ from mlxtend.feature_selection import SequentialFeatureSelector as sfs
 from mlxtend.plotting import plot_sequential_feature_selection as plot_sfs
 from sklearn.linear_model import LinearRegression,RidgeCV,LassoCV,ElasticNetCV
 
+# Objective
 target = 'area'
 
+# Load and describe data
 path = r'Datasets\forestfires.csv'
 df = pd.read_csv(path)
 print(df.shape)
 print(df.dtypes)
 print(df.describe().T)
+
+# Missing Value Treatment
+print(df.isna().sum().sum())
+
+# Exploratory Data Analysis
+plt.rcParams["figure.figsize"] = 9,5
+
+# Univariate
+plt.figure(figsize=(16,5))
+print("Skew: {}".format(df[target].skew()))
+print("Kurtosis: {}".format(df[target].kurtosis()))
+ax = sns.kdeplot(df[target],shade=True,color='g')
+plt.xticks([i for i in range(0,1200,50)])
+plt.show()
+ax = sns.boxplot(df[target])
 
